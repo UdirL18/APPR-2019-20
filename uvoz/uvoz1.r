d51666f6d4de372f5cb76f78a74b00c668f1964f
Kaja Ferjan <kaja.ferjan@student.fmf.uni-lj.si>
2019-12-17 08:44:26 +0100

    uvoz1

--- uvoz/uvoz1.r
+++ uvoz/uvoz1.r
@@ -1,9 +1,12 @@
 library(tidyverse)
+library(dplyr)
+library(readr)
 
 getwd()
-baku <- read.csv("2019_WCH_Baku_Results.csv", TRUE, ",",skip = 5,)
+baku <- read.csv2("X2019_WCH_Baku_Results", TRUE, ",",skip = 5,)
 View(baku)
 
+
 getwd()
-sofia <- read.csv("2018_WCH_Sofia_Results.csv", TRUE, ",")
+sofia <- read.csv2("2018_WCH_Sofia_Results", TRUE, ",")
 View(sofia)
