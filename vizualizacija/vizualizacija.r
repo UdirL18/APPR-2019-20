7b37468811c1b884e7918249919b3f2a46ccda1d
Lucija Udir <lucija.udir@student.fmf.uni-lj.si>
2020-01-16 10:02:03 +0100

    zemljevid

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -12,8 +12,18 @@ library(tidyverse)
 
 #graf ki prikazuje koliko ima kakšna tekmovalka točk pri težinah z rekvizitom, 
 #problem je da ne vemo kater rekvizit je to-barvamo
-ggplot(data = wcg) + 
-  geom_point(mapping = aes(x = tekmovalka, y = DA, color = rekvizit))
+graf.AD <- ggplot(data = wcg) + 
+  geom_point(mapping = aes(x = tekmovalka, y = DA, color = rekvizit)) + ggtitle("vrednosti težin z rekvizitom")
+
+#uvozimo zemljevid sveta
+zemljevid <- uvozi.zemljevid("https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip",
+                             
+                             "ne_50m_admin_0_countries",mapa="./zemljevidi") %>% fortify()
+
+
+
+
+
 
 # Uvozimo zemljevid.
 #zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
