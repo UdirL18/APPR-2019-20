250d4745ab1f76b4c32474823e642d0af1c8fc21
Lucija Udir <lucija.udir@student.fmf.uni-lj.si>
2020-01-23 09:59:47 +0100

    knjižnice za uvoz in vizalizacijo

--- lib/libraries.r
+++ lib/libraries.r
@@ -3,6 +3,12 @@ library(rvest)
 library(gsubfn)
 library(tidyr)
 library(shiny)
+library(readr)
+library(dplyr)
+library(tmap)
+library(ggplot2)
+library(grid)
+library(rworldmap)
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
