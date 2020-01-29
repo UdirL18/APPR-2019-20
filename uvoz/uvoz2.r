733dbc37ac9c194d5d076493b8daaa3bbfbb5008
UdirL18 <lucija.udir@student.fmf.uni-lj.si>
2020-01-30 00:15:47 +0100

    verjetno ne bom uporabila

--- uvoz/uvoz2.r
+++ uvoz/uvoz2.r
@@ -9,14 +9,22 @@ library(rvest)
 #stran %>% html_nodes(xpath='//*[@id="app"]/div[2]/div/div[2]/table') %>% html_table(header=TRUE, fill = TRUE) 
 
 
-uvoz.por.hoop <- function(por.hoop){
+uvoz.por.hoop <- function(){
   url <- "https://www.gymnastics.sport/site/events/results.php?idEvent=15889#filter"
   stran <- html_session(url) %>% read_html()
-  stran %>% html_nodes(xpath='//*[@id="app"]/div[2]/div/div[2]/table') %>% html_table(header=TRUE, fill = TRUE)
+  stran %>% html_nodes(xpath='//*[@id="app"]/div[2]/div/div[2]/table') %>% .[[1]] %>% html_table(header=TRUE, fill = TRUE)
 }  
 por.hoop <- uvoz.por.hoop()
 View(por.hoop)
 
 
+link <- "https://www.gymnastics.sport/site/events/results.php?idEvent=15889#filter"
+stran <- html_session(link) %>% read_html()
+vrstice <- stran %>% html_nodes(xpath='//*[@id="app"]/div[2]/div/div[2]/table') %>% html_text()
+csv <- gsub(" {2,}", ",", vrstice) %>% paste(collapse="") #zamenja, kjer sta vsaj 2 presledka z vejicami (v CSV)
+stolpci <- c("IZBRISI2","Sezon_v_ligi", "Ekipa", "Zmage_redni_del", "Porazi_redni_del", "Uspesnost_redni_del","Stevilo_playoffov", "Playoff_zmage", "Playoff_porazi","Playoff_uspesnost", "Zmage_serij","Porazi_serij","Uspesnost_serij","Nastopi_finale", "Zmage_finale","IZBRISI")
+
+podatki_ekipe <- read_csv(csv, locale=locale(encoding="UTF-8"), col_names=stolpci)
+podatki_ekipe <- podatki_ekipe[,-c(1,2,16)]
 
 
