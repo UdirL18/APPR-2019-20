3c767fcdf3f891ac77eae2ca2c6a41565e1f910d
UdirL18 <lucija.udir@student.fmf.uni-lj.si>
2020-12-04 00:24:48 +0100

    uvoz podatkov iz Kyiv RG rezultati.csv, združitev rezultatov iz evropskega prvenstva v eno tabelo ecg, združitev vseh rezultatov v tabelo induviduvalne (in okraski)

--- uvoz/uvoz1.r
+++ uvoz/uvoz1.r
@@ -30,11 +30,12 @@ library(naniar)
 #==============================================================================================================
 sl <- locale("sl", decimal_mark=".", grouping_mark=";") 
 
-
+#SOFIA
+#---------------------------------------------------------------------------------------------------------------
 sofia_hoop<- read_csv("podatki/sofia_hoop.csv", skip = 7, locale=locale(encoding="Windows-1250"))[,c(3,6,9,10,12:14)]
 colnames(sofia_hoop) <- c("tekmovalka","drzava","DB", "DA", "EA", "ET", "Pen.")
 
-  sofia_ball<- read_csv("podatki/sofia_ball.csv", skip = 7, locale=locale(encoding="Windows-1250"))[,c(3,6,9,10,12:14)]
+sofia_ball<- read_csv("podatki/sofia_ball.csv", skip = 7, locale=locale(encoding="Windows-1250"))[,c(3,6,9,10,12:14)]
 colnames(sofia_ball) <- c("tekmovalka","drzava","DB", "DA", "EA", "ET", "Pen.")
 
 sofia_clubs<- read_csv("podatki/sofia_clubs.csv", skip = 7, locale=locale(encoding="Windows-1250"))[,c(3,6,9,10,12:14)]
@@ -43,6 +44,10 @@ colnames(sofia_clubs) <- c("tekmovalka","drzava","DB", "DA", "EA", "ET", "Pen.")
 sofia_ribbon<- read_csv("podatki/sofia_ribbon.csv", skip = 7, locale=locale(encoding="Windows-1250"))[,c(3,6,9,10,12:14)]
 colnames(sofia_ribbon) <- c("tekmovalka","drzava","DB", "DA", "EA", "ET", "Pen.")
 
+
+
+#BAKU
+#------------------------------------------------------------------------------------------------------------------
 baku_hoop<- read_csv("podatki/baku_hoop.csv", skip = 7, locale=locale(encoding="Windows-1250"))[,c(3,6,9,10,12:14)]
 colnames(baku_hoop) <- c("tekmovalka","drzava","DB", "DA", "EA", "ET", "Pen.")
 
@@ -55,7 +60,35 @@ colnames(baku_clubs) <- c("tekmovalka","drzava","DB", "DA", "EA", "ET", "Pen.")
 baku_ribbon<- read_csv("podatki/baku_ribbon.csv", skip = 7, locale=locale(encoding="Windows-1250"))[,c(3,6,9,10,12:14)]
 colnames(baku_ribbon) <- c("tekmovalka","drzava","DB", "DA", "EA", "ET", "Pen.")
 
+
+
+#KIJEV
+#-----------------------------------------------------------------------------------------------------------------
+kijev_hoop<- 
+  read_csv("podatki/kyiv RG rezultati.csv", skip = 6, n_max= 25, locale=locale(encoding="Windows-1250"))[,c(2:7)]
+colnames(kijev_hoop) <- c("tekmovalka","drzava","E", "D", "Pen.", "koncna_ocena")
+
+kijev_ball<- 
+  read_csv("podatki/kyiv RG rezultati.csv", skip = 43, n_max= 25, locale=locale(encoding="Windows-1250"))[,c(2:7)]
+colnames(kijev_ball) <- c("tekmovalka","drzava","E", "D", "Pen.", "koncna_ocena")
+
+kijev_clubs<- 
+  read_csv("podatki/kyiv RG rezultati.csv", skip = 80, n_max= 25, locale=locale(encoding="Windows-1250"))[,c(2:7)]
+colnames(kijev_clubs) <- c("tekmovalka","drzava","E", "D", "Pen.", "koncna_ocena")
+
+kijev_ribbon<- 
+  read_csv("podatki/kyiv RG rezultati.csv", skip = 116, n_max= 25, locale=locale(encoding="Windows-1250"))[,c(2:7)]
+colnames(kijev_ribbon) <- c("tekmovalka","drzava","E", "D", "Pen.", "koncna_ocena")
+
+
+
+#==============================================================================================================
+#DODAJANJE STOLPCEV
+#==============================================================================================================
 #vidimo da bi vse to lahko združili v eno tabelo, zraven imena bi morali napisati še tekmo in rekvizit.
+
+#SOFIA
+#-------------------------------------------------------------------------------------------------------------
 sofia_hoop$rekvizit<-"hoop" 
 sofia_hoop$tekma<-"Sofia"
 
@@ -68,6 +101,10 @@ sofia_clubs$tekma<-"Sofia"
 sofia_ribbon$rekvizit<-"ribbon" 
 sofia_ribbon$tekma<-"Sofia"
 
+
+
+#BAKU
+#------------------------------------------------------------------------------------------------------------
 baku_hoop$rekvizit<-"hoop" 
 baku_hoop$tekma<-"Baku"
 
@@ -82,7 +119,24 @@ baku_ribbon$tekma<-"Baku"
 
 
 
+#KIJEV
+#-------------------------------------------------------------------------------------------------------------
+kijev_hoop$rekvizit<-"hoop" 
+kijev_hoop$tekma<-"Kijev"
 
+kijev_ball$rekvizit<-"ball" 
+kijev_ball$tekma<-"Kijev"
+
+kijev_clubs$rekvizit<-"clubs" 
+kijev_clubs$tekma<-"Kijev"
+
+kijev_ribbon$rekvizit<-"ribbon" 
+kijev_ribbon$tekma<-"Kijev"
+
+
+#=============================================================================================================
+#ZDRUŽITEV SVETOVNIH POKALOV V SKUPNO TABELO wcg
+#=============================================================================================================
 # uporabimo knjižnico library(naniar), replace_with_na_at nadomesti spremenljivke z NA če je dosežen pogoj
 # .vars = na katerem stolpcu uporabimo zamenjavo, 
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!kaj je ~.x
@@ -99,7 +153,29 @@ View(wcg)
 
 
 
+#=============================================================================================================
+#ZDRUŽITEV EVROPSKEGA PRVENSTVA KIJEV V SKUPNO TABELO ecg
+#=============================================================================================================
+ecg <- rbind(kijev_hoop, kijev_ball, kijev_clubs, kijev_ribbon)
+
+#preuredimo še vrstni red stolpcev
+ecg <- ecg[c('tekmovalka', 'drzava', 'tekma', 'rekvizit', 'E', 'D', 'Pen.', 'koncna_ocena')]
+
+View(ecg)
 
 
 
+#=============================================================================================================
+#ZDRUŽITEV TABEL wcg IN ecg V SKUPNO TABELO induvidualne
+#=============================================================================================================
+#če bi želeli združit tabeli bi morali v wcg seštet EA 7 in  ET 8 da dobimo E nato DB in DA da dobimo D
+wcg_D_E <- wcg
+wcg_D_E$E <- wcg_D_E$EA + wcg_D_E$ET
+wcg_D_E$D <- wcg_D_E$DB + wcg_D_E$DA
+wcg_D_E$koncna_ocena <- wcg_D_E$D + 10 + wcg_D_E$E + wcg_D_E$Pen.
+wcg_D_E <- wcg_D_E[c('tekmovalka', 'drzava', 'tekma', 'rekvizit', 'E', 'D', 'Pen.', 'koncna_ocena')]
+View(wcg_D_E)
+
+induvidualne <- rbind(ecg, wcg_D_E)
+View(induvidualne)
 
