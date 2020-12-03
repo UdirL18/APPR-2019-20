6ff13ab285872fd3a1b7f3621d97359fcabd81a1
UdirL18 <lucija.udir@student.fmf.uni-lj.si>
2020-12-03 23:35:51 +0100

    uvoz rezultatov iz wikipedije in uvoz iz csv-ja

---
+++ uvoz/uvoz_html.r
@@ -0,0 +1,121 @@
+#UVOZ PODATKOV IZ SPLETA
+
+#KNJIŽNICE------------------------------------------------------------------------------------------------------
+require(tidyr)
+require(readr)
+require(dplyr)
+require(rvest) #za spletne strani, omogoča premikanje po značkah
+require(XML)
+#---------------------------------------------------------------------------------------------------------------
+
+#BAKU
+#---------------------------------------------------------------------------------------------------------------
+#v spremenljivko url si shranimo spletno povezavo
+url_baku_skupinske <- "https://en.wikipedia.org/wiki/2019_Rhythmic_Gymnastics_World_Championships" 
+
+tabela_baku_skupinske <- read_html(url_baku_skupinske) #iz knjižnice rvest
+#če želimo shraniti
+#write_html(stran_mlad_dp,"stran_mlad_dp.html")
+
+
+
+#ZOGE
+#---------------------------------------------------------------------------------------------------------------
+#html_nodes iz rvest-izbiranje delov htmlja, xpath-poseben jezik za zapisovanje poti do značke
+tabela_baku_skupinske %>%
+  html_nodes(xpath = "//table[@class='wikitable sortable']") %>% 
+  .[[9]] %>% 
+  html_table() %>% 
+  rename(drzava = 2, E = 3, D = 4, koncna_ocena = 6) %>% 
+  replace_na(list(Pen. = 0)) %>% #NA v stolpcu Pen. spremenim v 0
+  select(-1) %>% #izbrišem prvi stolpec(uvrstitev) ker to lahko izračunamo sami
+  mutate(rekvizit = "5 žog") %>%
+  mutate(tekma = "Baku") -> tabela_baku_skupinske_zoge
+  #View() %>%
+  #sapply(class) # rank:integer, drzava: caracter, E: num, D num, pen.:num, total:num 
+
+
+
+#KIJI + OBROCI 
+#-----------------------------------------------------------------------------------------------------------------
+ tabela_baku_skupinske %>%
+  html_nodes(xpath = "//table[@class='wikitable sortable']") %>% 
+  .[[10]] %>% 
+  html_table() %>% 
+  rename(drzava = 2, E = 3, D = 4, koncna_ocena = 6) %>% 
+  replace_na(list(Pen. = 0)) %>% #NA v stolpcu Pen. spremenim v 0
+  select(-1) %>% #izbrišem prvi stolpec(uvrstitev) ker to lahko izračunamo sami
+  mutate(rekvizit = "3 obroči + 4 kiji") %>%
+  mutate(tekma = "Baku") -> tabela_baku_skupinske_kiji_obroci 
+  #View() %>%
+  #sapply(class) # rank:integer, drzava: caracter, E: num, D num, pen.:num, total:num 
+
+
+
+
+#SKUPNA TABELA
+#------------------------------------------------------------------------------------------------------------------
+wcg_baku_skupinske <- rbind(tabela_baku_skupinske_kiji_obroci, tabela_baku_skupinske_zoge)
+View(wcg_baku_skupinske)
+
+
+
+
+#==================================================================================================================
+#KIJEV
+sl <- locale("sl", decimal_mark=".", grouping_mark=";") 
+
+kijev_zoge <-
+  read_csv("podatki/Kyiv RG rezultati.csv", skip = 153, n_max = 6, locale=locale(encoding="Windows-1250"))[,c(3:7)]
+
+kijev_zoge %>% 
+  rename(drzava = 1, E = 2, D = 3, Pen.= 4, koncna_ocena = 5) %>% #preimenujemo stolpce
+  mutate(rekvizit = "5 žog") %>% #dodamo dva stolpca
+  mutate(tekma = "Kijev") -> kijev_zoge
+
+
+
+#------------------------------------------------------------------------------------------------------------------
+kijev_kiji_obroci <-
+  read_csv("podatki/Kyiv RG rezultati.csv", skip = 170, n_max = 6, locale=locale(encoding="Windows-1250"))[,c(3:7)]
+
+kijev_kiji_obroci %>% 
+  rename(drzava = 1, E = 2, D = 3, Pen.= 4, koncna_ocena = 5) %>% #preimenujemo stolpce
+  mutate(rekvizit = "3 obroči + 4 kiji") %>% #dodamo dva stolpca
+  mutate(tekma = "Kijev") -> kijev_kiji_obroci 
+
+
+
+#SKUPNA TABELA
+#------------------------------------------------------------------------------------------------------------------
+ecg_kijev_skupinske <- rbind(kijev_kiji_obroci, kijev_zoge)
+View(ecg_kijev_skupinske)
+
+
+
+#================================================================================================================
+#SKUPNA TABELA SKUPINSKIH VAJ
+skupinske <- rbind(wcg_baku_skupinske, ecg_kijev_skupinske)
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#ni mi všeč ker mam nekje države poimenovane z kraticam nekje pa s celim imenom
+#wcg_skupinske$drzava <- recode(wcg$drzava 
+#                     ,'BUL' = 'Bulgaria'
+#                     ,'ISR' = 'Israel'
+#                     ,'ITA' = 'Italy'
+#                     ,'JPN' = 'Japan'
+#                     ,'RUS' = 'Russia'
+#                     ,'UKR' = 'Ukraine'
+#                     ,'AZE' = 'Azerbaijan'
+#                     ,'CHN' = 'China'
+#                     ,'BLR' = 'Belarus')
+View(skupinske)
+
+
+################################################################################################################
+#KAKO SMO TO UVOZILI NA PREDAVANJIH
+url_wiki <- "https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)"
+wiki <- read_html(url_wiki)
+wiki %>% html_nodes(xpath = "//table[@class = 'wikitable sortable']") %>% 
+  .[[1]] %>% html_table() %>% View()
+
