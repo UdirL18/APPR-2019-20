d76d8056f21d74b0e70c5b29a25d3604a7022a85
UdirL18 <lucija.udir@student.fmf.uni-lj.si>
2021-03-06 17:02:31 +0100

    Dodala vse knjižnice ki sem jih uporabila.

--- lib/libraries.r
+++ lib/libraries.r
@@ -1,7 +1,5 @@
 library(knitr)
-library(rvest)
 library(gsubfn)
-library(tidyr)
 library(shiny)
 library(readr)
 library(dplyr)
@@ -9,6 +7,26 @@ library(tmap)
 library(ggplot2)
 library(grid)
 library(rworldmap)
+library(tidyr) #za funkciji gather() in spread()
+library(naniar)
+library(rvest) #za spletne strani, omogoča premikanje po značkah
+library(XML)
+library(tidyverse) #mutate
+library(scales) #izračun deleža
+library(wesanderson) #barva grafa
+library(reshape2) #za melt pri grafu 5
+library(rgdal)
+library(rgeos)
+library(mosaic)
+library(maptools)
+library(munsell)
+library(StatMeasures)
+library(magrittr)
+library(ggmap)
+library(stringr)
+library(NbClust) #Za določanje skupin
+library(ggdendro) #za dendrograme
+library(ggrepel) #za overlapping
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
-source("lib/uvozi.zemljevid.r", encoding="UTF-8")
+#source("lib/uvozi.zemljevid.r", encoding="UTF-8")
